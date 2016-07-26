6846bd2807487f7969b91a33ba82aed5331c9b4d
Eric Holscher <eric@ericholscher.com>
2016-07-26 15:01:44 -0700

    Few more blog config options

--- conf.py
+++ conf.py
@@ -53,6 +53,8 @@ blog_locations = {
 }
 blog_default_location = 'PDX'
 blog_feed_archives = True
+blog_feed_fulltext = True
+blog_feed_length = 5
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
